043138e58b1220f1fbe3c20f0bddb5e208818a8a
Deepak9arts <coc9artsdragon.dk@gmail.com>
2024-10-01 00:25:32 +0530

    "npm"

---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+
+
+import generatName from 'sillyname';
+var sillyname=generatName();
+console.log(sillyname); 
+
+
+import {randomSuperhero} from 'superheroes';
+var superheroname=randomSuperhero();
+console.log(superheroname);
+
+
